ec422489d30dbe95f5a87caa6fc7a3b24e11c7a4
Calvin0630 <cg7057705@gmail.com>
2015-05-02 16:05:11 -0400

    PathFinding Works!!!!1!!1!11

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,29 +6,31 @@ public class Spawner : MonoBehaviour {
     List<Enemy> FirstList;
     public Vector3[] FirstPath;
     public Enemy Enemy;
+    Enemy obj;
 
-	public float speed;
+    public float speed;
 
     // Use this for initialization
-	void Start () {
-	    FirstList = new List<Enemy>();
+    void Start() {
+        FirstList = new List<Enemy>();
         //goes from top right to left middle
         StartCoroutine(SpawnCoroutine1(FirstPath[0], FirstList));
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {        
-                foreach (Enemy obj in FirstList) {
-                    obj.transform.position = Vector3.MoveTowards(obj.transform.position, FirstPath[obj.GetDestination()], speed);
-                    if (obj.transform.position == FirstPath[obj.GetDestination()] && (obj.GetDestination() < FirstPath.Length )) {
-                        obj.IncrementDesination();
-                        if (obj.GetDestination() == FirstPath.Length) {
-                            FirstList.Remove(obj);
-                            Destroy(obj);
-                    }
+    // Update is called once per frame
+    void Update() {
+        
+        for (int i = 0; i < FirstList.Count; i++) {
+            obj = FirstList[i];
+            obj.transform.position = Vector3.MoveTowards(obj.transform.position, FirstPath[obj.GetDestination()], speed);
+            if (obj.transform.position == FirstPath[obj.GetDestination()] && (obj.GetDestination() < FirstPath.Length)) {
+                obj.IncrementDesination();
+                if (obj.GetDestination() == FirstPath.Length) {
+                    obj.SetDestination(0);
                 }
-                }
-	}
+            }
+        }
+    }
 
     IEnumerator SpawnCoroutine1(Vector3 Location, List<Enemy> list) {
         int Count = 0;
