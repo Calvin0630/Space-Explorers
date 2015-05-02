247e0e5fbf402af995ad317966b462624108a399
Calvin0630 <cg7057705@gmail.com>
2015-05-01 21:33:25 -0400

    minor changes to Spawner Script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour {
-    List<GameObject> list;
+    List<GameObject> FirstList;
     Vector3[] FirstPath;
     public GameObject Enemy;
     // Use this for initialization
 	void Start () {
-	    list = new List<GameObject>();
+	    FirstList = new List<GameObject>();
         //goes from top right to left middle
         FirstPath = new Vector3[] { new Vector3(12, 5, 0), new Vector3(-12, 0, 0) };
 	}
@@ -16,7 +16,7 @@ public class Spawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Spawn1(new Vector3(12, 5));
-        foreach (GameObject obj in list) {
+        foreach (GameObject obj in FirstList) {
             obj.transform.position = Vector3.MoveTowards(obj.transform.position, FirstPath[1], .1f);
         }
 	}
@@ -26,7 +26,7 @@ public class Spawner : MonoBehaviour {
         int Count = 0;
         if (Time.time % 1 < .01f && Count <= 5) {
             GameObject clone = (GameObject)Instantiate(Enemy, Location, Quaternion.identity);
-            list.Add(clone);
+            FirstList.Add(clone);
         }
     }
 }
