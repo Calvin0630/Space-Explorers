65565bcfc8725aab062c7fbb11930319ecb9b6a7
Calvin0630 <cg7057705@gmail.com>
2015-05-02 15:27:41 -0400

    Revert "spawner"
    This reverts commit e5acb33cf07ce67c536b04b2bfa0732df4721def.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class Spawner : MonoBehaviour {
     List<Enemy> FirstList;
     public Vector3[] FirstPath;
-    public Enemy Enemy;
+    public GameObject Enemy;
 
 	public float speed;
 
@@ -20,7 +20,7 @@ public class Spawner : MonoBehaviour {
 	void Update () {        
                 foreach (Enemy obj in FirstList) {
                     obj.transform.position = Vector3.MoveTowards(obj.transform.position, FirstPath[obj.GetDestination()], speed);
-                    Debug.Log(obj.GetDestination());
+                    if (obj.GetComponent<Rigidbody2D>().velocity == Vector2.zero) {
                         obj.IncrementDesination();
                        
                     }
