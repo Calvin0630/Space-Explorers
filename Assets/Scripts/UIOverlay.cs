1a0da06df2c82a87a7410f522b969dbd19653b95
David Tenty <dtenty@ryerson.ca>
2015-05-01 21:49:16 -0400

    Base UI

---
+++ Assets/Scripts/UIOverlay.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIOverlay : MonoBehaviour {
+
+	public Vector2 pos = new Vector2(10,25);
+	public Vector2 size = new Vector2(500,20);
+	public Image BarE;
+	public Image BarF;
+	public bool Survival;
+	public bool Boss;
+	public float BossHP=500f;
+	public float BossMHP=500f;
+	float BossRHP;
+	float timer = 0.0f;
+	void OnGUI () {
+		if (Survival == true) {
+			timer += Time.deltaTime;
+			int Second = (int)Mathf.Floor (timer % 60);
+			int Minute = (int)Mathf.Floor (timer / 60);
+			//string Sec = Second.ToString ();
+			//string Min = Minute.ToString ();
+			string Tine = string.Format ("{0:00}:{1:00}", Minute, Second);
+			GUI.Box (new Rect (10, 10, 100, 50), Tine);
+		}
+		if (Boss == true) {
+			BossRHP = BossHP / BossMHP;
+			
+			//draw the filled-in part:
+			BarF.rectTransform.localPosition=new Vector3(0,0,0);
+			//BarE.rectTransform.localScale*BossRHP;
+			
+			GUI.EndGroup ();
+		}
+	}
+}
