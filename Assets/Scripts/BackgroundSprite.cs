12ba133a1146391c46ad89e98cd8d81a17448575
David Tenty <dtenty@ryerson.ca>
2015-05-03 16:08:37 -0400

    Backgroud scrawl

---
+++ Assets/Scripts/BackgroundSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSprite : MonoBehaviour {
+	public float scrollRate;
+	private Frame frame;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<Rigidbody2D> ().velocity = scrollRate * Vector3.down;
+		frame = GameObject.Find("Frame").GetComponent<Frame>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < frame.BottomCube.transform.position.y) {
+			transform.position = frame.TopCube.transform.position;
+		}
+	}
+}
