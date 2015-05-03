c61107c8a592af37b2194febab359ef8b0abb96f
David Tenty <dtenty@ryerson.ca>
2015-05-03 15:11:03 -0400

    Build straight shooter

---
+++ Assets/Scripts/StraightShooter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StraightShooter : Enemy {
+
+	protected override void ActiveUpdate() {
+		base.ActiveUpdate ();
+		if (ShotCounter == 0) {
+			GameObject clone = (GameObject)Instantiate(EnemyProjectile, transform.position, Quaternion.identity);
+			//sets velocity to shoot at the player plus a random vector
+			clone.GetComponent<Rigidbody2D>().velocity = SpeedOfBullet * Vector3.down;
+		}
+	}
+}
