e04d4f467b1bb5b1c0e248f3f5e6ed850903ed40
David Tenty <dtenty@ryerson.ca>
2015-05-03 13:21:45 -0400

    Model changes

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	public GameObject bg1,bg2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
